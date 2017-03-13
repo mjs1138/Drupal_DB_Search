a511506690af11a9f6a3f957308c35b598591fcf
Mike <mike@scllc.us>
2017-03-13 09:25:27 -0400

    full working

--- src/Controller/DisplayDBTableColumnValues.php
+++ src/Controller/DisplayDBTableColumnValues.php
@@ -3,6 +3,7 @@
 namespace Drupal\drupal_db_search\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 use Drupal\Core\Database\Driver\mysql\Connection;
 
 /**
@@ -14,45 +15,30 @@ class DisplayDBTableColumnValues extends ControllerBase
     public function displayDbTableColumnValues()
     {
 
+
         $targetTable = $_SESSION['DrupalDbSearch']['FormValues']['table'];
         $targetColumn = $_SESSION['DrupalDbSearch']['FormValues']['column'];
 
+
+
         $dbList = $this->_getListOfDbS();
-//        $targetDbS = $this->_getListOfTargetDbS($dbList, $targetTable);
+        $targetDbS = $this->_getListOfTargetDbS($dbList, $targetTable);
+        $dBValueS = $this->_getColumnValueS($targetDbS, $targetTable, $targetColumn);
 
-
-
-
-
-        $query = \Drupal::database()->select('users_field_data', 'u');
-        $query->fields('u', ['uid', 'name', 'mail']);
-        $results = $query->execute()->fetchAll();
-
-        $header = [
-            'userid' => t('User id'),
-            'Username' => t('username'),
-            'email' => t('Email'),
-        ];
-
-        // Populate the rows.
-        $rows = array();
-        foreach ($results as $row) {
-            $rows[] = array('data' => array(
-                'userid' => $row->uid,     // 'userid' was the key used in the header
-                'Username' => $row->name, // 'Username' was the key used in the header
-                'email' => $row->mail,    // 'email' was the key used in the header
-            ));
+        foreach ($dBValueS as $dbName => $dbColDataS) {
+            foreach ($dbColDataS as $data) {
+                $tableRowS[] = array($dbName, $data);
+            }
         }
 
-        $form['table'] = [
-            '#type' => 'table',
-            '#header' => $header,
-            '#rows' => $rows,
-            '#empty' => t('No users found!!!'),
-        ];
+        $table = array(
+            '#theme' => 'table',
+            '#header' => array(t('Databse'), t('Table: ' . $targetTable)),
+            '#rows' => $tableRowS,
+        );
 
+        return $table;
 
-        return $form;
     }
 
     function _getListOfDbS()
@@ -66,14 +52,25 @@ class DisplayDBTableColumnValues extends ControllerBase
     {
         $count = 0;
         $dbTargetDbList = array();
-        $pdo = new PDO(sprintf('mysql:host=%s', DB_HOST), DB_USER, DB_PASS); //PDO without selecting DB
+
+//        $pdo = new \PDO(sprintf('mysql:host=%s', DB_HOST), DB_USER, DB_PASS); //PDO without selecting DB
+
+        $connection = \Drupal::database();
         foreach ($dbList as $db) {
-            $pdo->query('USE ' . $db); // Select DB
-            if ($result = $pdo->query('SHOW TABLES FROM ' . $db)) {
-                $tableS = $result->fetchAll();
-                foreach ($tableS as $table) {
+            $dbName = $db->Database;
+
+            $connection->query("USE  `$db->Database`"); // Select DB
+
+            if ($result = $connection->query("SHOW TABLES FROM `$db->Database`")) {
+//                $tableS = $result->fetchCol();
+//                $tableS = $result->fetchAllKeyed("fun");
+//                $tableS = $result->fetchAll();
+//                $tableS = $result->fetchAll(\PDO::FETCH_ASSOC);
+                $tableS = $result->fetchAll(\PDO::FETCH_BOTH);
+//                $tableS = $result->fetch();
+                foreach ($tableS as $key => $table) {
                     if ($table[0] == $targetTable) {
-                        $dbTargetDbList[] = $db;
+                        $dbTargetDbList[] = $db->Database;
                     }
                     $count++;
                 }
@@ -82,5 +79,23 @@ class DisplayDBTableColumnValues extends ControllerBase
         return $dbTargetDbList;
     }
 
+    function _getColumnValueS($targetDbS, $targetTable, $targetColumn)
+    {
+        $count = 0;
+        $colValueS = array();
+
+        $connection = \Drupal::database();
+        foreach ($targetDbS as $db) {
+//            $dbName = $db->Database;
+            $connection->query("USE  `$db`"); // Select DB
+            $result = $connection->query("SELECT $targetColumn FROM $targetTable");
+            $valueS = $result->fetchCol();
+
+            foreach ($valueS as $value) {
+                $colValueS[$db][] = $value;
+            }
+        }
+        return $colValueS;
+    }
 
 }
